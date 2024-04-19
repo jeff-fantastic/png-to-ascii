6c070326dec8977531e410473f52d756bd96a4d8
jefftastic <48529067+jeff-fantastic@users.noreply.github.com>
2024-04-19 12:51:14 -0400

    wrap up some things for 1.0

---
+++ PNGtoASCII/src/main/java/com/jefftastic/pngtoascii/Launcher.java
@@ -0,0 +1,11 @@
+package com.jefftastic.pngtoascii;
+
+/**
+ * Wrapper class that launches the actual main class
+ */
+
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
