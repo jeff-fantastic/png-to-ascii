a81a931532e34585005a8b3b33160ceeee284f2f
jefftastic <48529067+jeff-fantastic@users.noreply.github.com>
2024-04-23 12:39:42 -0400

    preference class

--- src/main/java/com/jefftastic/pngtoascii/Preferences.java
+++ src/main/java/com/jefftastic/pngtoascii/Preferences.java
@@ -1,5 +1,8 @@
 package com.jefftastic.pngtoascii;
 
+import java.io.*;
+import java.util.*;
+
 /**
  * Handles loading, reading, and saving of
  * user preference data at runtime.
@@ -7,4 +10,91 @@ package com.jefftastic.pngtoascii;
  */
 
 public class Preferences {
+    // Declare enums / finals
+    public enum PreferenceCharSet   { Generic, Block, Kanji }
+    public enum PreferenceImageLimit { s512, s1024, sNone }
+    private static final String PROPERTIES_PATH = "." + File.separator + "user-config.p";
+
+    /**
+     * Holds all data related to user preference.
+     * Stored as strings, since the Properties
+     * class can only write and parse string data.
+     * Data conversion must be handled by other parts
+     * of the program that wish to use this data.
+     * @see Properties
+     */
+    public static Map<String, String> preference = new HashMap<>();
+
+    /**
+     * Initializes preference map
+     */
+    public Preferences() {
+        try {
+            if (!loadPreferences())
+                savePreferences();
+        } catch (IOException e) {
+            /*
+             Since we check if the preference file exists,
+             it can be assumed the error likely involves the
+             new default file not being saved properly. In the
+             case of this event, just load default values.
+             */
+            e.printStackTrace(System.out);
+            assignDefaults();
+        }
+    }
+
+    /**
+     * Saves user preferences to a properties file
+     */
+    public static void savePreferences() throws IOException {
+        // Create properties
+        Properties prop = new Properties();
+
+        // Assign all values
+        for (String key : preference.keySet())
+            prop.setProperty(key, preference.get(key));
+
+        // Save to file
+        prop.store(new FileWriter(PROPERTIES_PATH), "User Preferences Data");
+    }
+
+    /**
+     * Loads user preferences from a properties file,
+     * but only if one exists. Returns false if the
+     * preference file could not be located.
+     */
+    public static boolean loadPreferences() throws IOException {
+        // Determine if properties file exists
+        Properties prop = new Properties();
+        File path = new File(PROPERTIES_PATH);
+        if (!path.exists())
+            return false;
+
+        // Otherwise, lets load from file
+        preference.clear();
+        prop.load(new FileReader(path.getAbsolutePath()));
+        for (String property : prop.stringPropertyNames()) {
+            /*
+             * If the property doesn't already exist in
+             * the preference map, it should be discarded
+             */
+            if (!preference.containsKey(property))
+                continue;
+            preference.put(property, prop.getProperty(property));
+        }
+        return true;
+    }
+
+    /**
+     * Initializes preference HashMap with
+     * default values.
+     */
+    private static void assignDefaults() {
+        preference.clear();
+        preference.put("charSet", PreferenceCharSet.Generic.toString());
+        preference.put("imageLimit", PreferenceImageLimit.sNone.toString());
+        preference.put("font", "Courier");
+        preference.put("grayscale", "true");
+    }
 }
--- src/main/java/com/jefftastic/pngtoascii/PreferencesController.java
+++ src/main/java/com/jefftastic/pngtoascii/PreferencesController.java
@@ -1,10 +1,81 @@
 package com.jefftastic.pngtoascii;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
 /**
  * Wrangles what is input into the preferences
  * GUI and sends that data to the preferences manager
  * @see Preferences
  */
 
-public class PreferencesController {
+public class PreferencesController implements Initializable {
+
+    @FXML
+    public ChoiceBox<String> characterSet;
+    @FXML
+    public ChoiceBox<String> imageSize;
+    @FXML
+    public ChoiceBox<String> fontSet;
+
+    /**
+     * Initializes various fields.
+     * @param url unused
+     * @param resourceBundle unused
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Initialize font box
+        List<String> list = Font.getFontNames();
+        fontSet.getItems().addAll(list);
+
+        // Initialize image sizes
+        imageSize.getItems().addAll("512", "1024", "None");
+
+        // Initialize character set
+        characterSet.getItems().addAll("Generic", "Block", "Kanji");
+    }
+
+    /**
+     * Called when the users font choice has changed.
+     */
+    private void fontChanged() {
+
+    }
+
+    /**
+     * Fired when grayscale option is toggled.
+     * @param actionEvent
+     */
+    public void grayscaleToggled(ActionEvent actionEvent) {
+
+    }
+
+    /**
+     * Fired when apply button is pressed.
+     */
+    protected void applyPressed() {
+        // Send field information to preferences class
+    }
+
+    /**
+     * Fired when cancel button is pressed.
+     */
+    @FXML
+    protected void cancelPressed() {
+        // Close self
+        Stage stage = (Stage) characterSet.getScene().getWindow();
+        stage.close();
+    }
 }
