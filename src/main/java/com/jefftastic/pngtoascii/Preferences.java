15f53a0d85a31fef334388f688a33fb342e8face
jefftastic <48529067+jeff-fantastic@users.noreply.github.com>
2024-04-23 11:17:29 -0400

    create javafx events

---
+++ src/main/java/com/jefftastic/pngtoascii/Preferences.java
@@ -0,0 +1,10 @@
+package com.jefftastic.pngtoascii;
+
+/**
+ * Handles loading, reading, and saving of
+ * user preference data at runtime.
+ * @see PreferencesController
+ */
+
+public class Preferences {
+}
---
+++ src/main/java/com/jefftastic/pngtoascii/PreferencesController.java
@@ -0,0 +1,10 @@
+package com.jefftastic.pngtoascii;
+
+/**
+ * Wrangles what is input into the preferences
+ * GUI and sends that data to the preferences manager
+ * @see Preferences
+ */
+
+public class PreferencesController {
+}
